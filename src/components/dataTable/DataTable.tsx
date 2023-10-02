dac8842cab0f20b98475e66e10000a77733f736b
Bhavesh Thakur <102952918+bhavesh-348@users.noreply.github.com>
2023-10-02 19:31:32 +0530

    charts and grid table added
    charts and grid table added

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import {
+  DataGrid,
+  GridColDef,
+  GridToolbar,
+} from "@mui/x-data-grid";
+import "./dataTable.scss";
+import { Link } from "react-router-dom";
+
+interface DataRow {
+  id: number;
+  img: string;
+  lastName: string;
+  firstName: string;
+  email: string;
+  phone: string;
+  createdAt: string;
+  verified?: boolean; 
+}
+
+type Props = {
+  columns: GridColDef[];
+  rows: DataRow[]; 
+  slug: string;
+};
+
+const DataTable = (props: Props) => {
+  const [dataRows, setDataRows] = useState<DataRow[]>(props.rows);
+
+  const handleDelete = (id: number) => {
+    const updatedRows = dataRows.filter((row) => row.id !== id);
+    setDataRows(updatedRows);
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="" />
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={dataRows}
+        columns={[...props.columns, actionColumn]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
